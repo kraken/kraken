973cdb3fd109e940ec9f06ba215e092217409982
Ryan Mohr <ryan.mohr@gmail.com>
2015-02-10 14:57:43 -1000

    Clean up density test

--- test/metrics/density-test.js
+++ test/metrics/density-test.js
@@ -2,16 +2,17 @@ var assert = require("assert");
 var Kraken = require("../kraken");
 
 suite("metrics", function() {
-  test("density", function() {
-    var graph = Kraken()
-      .add("a")
-      .add("b")
-      .add("c")
-      .connect("a", "b")
-      .connect("a", "c");
+  suite("density", function() {
+    test("simple", function() {
+      var graph = Kraken()
+        .add("a")
+        .add("b")
+        .add("c")
+        .connect("a", "b")
+        .connect("a", "c")
+        .calc("density");
 
-    assert.equal(graph.get("a").density(), 1 / 3);
-    assert.equal(graph.get("b").density(), 0.5);
-    assert.equal(graph.get("c").density(), 0.5);
-  });
-});
+      assert.deepEqual(graph.collect("density"), {a: 1/3, b: 0.5, c: 0.5});
+    })
+  })
+})
